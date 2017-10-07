fbe766ced805760f1b52a741e92abab36d6b3fbc
Jorge Rodrigues Gomes Vaz Filho <jorge.rodrigues.gvf@gmail.com>
2017-10-07 18:15:08 -0300

    Up until 'Visibilidade das Abas (Parte 2)'

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,18 @@
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
   <div className='wrapper'>
-    <h1>App</h1>
+    <Header />
+    <Sidebar />
+    <div className='content-wrapper'>
+      <Routes />
+    </div>
+    <Footer />
   </div>
 )
